b02dc8844d5979a2b7eb264f539f606604e98059
niccostantini <costantiniccolo@gmail.com>
2024-07-17 16:46:50 +0200

    Solved ex. 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(str, n) {
+    if (n<0) return "ERROR";
+    let output = "";
+    for (let i = 0; i<n; ++i) {
+         output += str;
+        }
+    return output
 };
-
 // Do not edit below this line
 module.exports = repeatString;
