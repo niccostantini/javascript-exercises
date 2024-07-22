606e93010e6becd5d3f36b9d5c56634352fb24d1
niccostantini <costantiniccolo@gmail.com>
2024-07-22 16:28:44 +0200

    Solve ex 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    string = string.toLowerCase().replace(/[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~\s]/g , "");
+    let newString = string.split('').reverse().join('');
+    return string === newString
 };
 
 // Do not edit below this line
