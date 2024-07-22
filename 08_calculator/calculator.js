8e5d4ad554d62d7e36843bf11168ef6d9d1f5617
niccostantini <costantiniccolo@gmail.com>
2024-07-22 16:02:18 +0200

    solve exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,44 @@
-const add = function() {
+const add = function(a, b) {
+    return a + b;
+};
+
+const subtract = function(a, b) {
+  return a - b;
 	
 };
 
-const subtract = function() {
+const sum = function(array) {
+
+  let total = 0;
+  for (i = 0; i < array.length; i++) {
+    total += array[i];
+  }
+  return parseInt(total);
+
+};
+
+const multiply = function(array) {
+  product = array.reduce((a,b) => a * b);
+  return product
+};
+
+const power = function(a,b) {
+  return a ** b;
 	
 };
 
-const sum = function() {
-	
-};
+const factorial = function(n) {
+	let array = []; 
+  if (n == 0 || n == 1) return 1
+  else { 
+    for (i=0; i < n; i++) {
+        array.push(n-i)
+    }
 
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
-};
+    let result = array.reduce((a,b) => a*b);
+    return result
+  }
+  };
 
 // Do not edit below this line
 module.exports = {
