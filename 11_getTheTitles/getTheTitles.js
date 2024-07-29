18a7df64792ead7699ee854101bb598e159f9766
niccostantini <costantiniccolo@gmail.com>
2024-07-30 01:09:42 +0200

    Solved ex. 12

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,17 @@
-const getTheTitles = function() {
+const books = [
+    {
+      title: 'Book',
+      author: 'Name'
+    },
+    {
+      title: 'Book2',
+      author: 'Name2'
+    }
+  ]
 
+const getTheTitles = function(books) {
+    let titles = books.map(book => book.title);
+    return titles
 };
 
 // Do not edit below this line
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,9 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(obj) {
+    obj.map(function(item) {
+        if(!item.yearOfDeath) {item.yearOfDeath = 2024}
+    })
+    sorted = obj.sort((a, b) => (a.yearOfBirth - a.yearOfDeath) < (b.yearOfBirth - b.yearOfDeath) ? -1 : +1)
+    return sorted[0];
 };
 
 // Do not edit below this line
